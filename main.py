fec8d2bb2dc5e1cc90a364034b65799b0e204b8e
Mattias Holmström <mattias.holmstrom11@gmail.com>
2022-11-16 13:50:00 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 13:00:19 2022
+
+@author: mattias
+"""
+
+"""Implementation of the Lewenberg Marquardt algorithm, optimization method for nonlinear least squares"""
+
+import numpy as np
+import scipy
+import pandas as pd
+import math
+
+def levmarq(func, x0):
+    
+    
+
+    return NotImplemented
+
+
+
+t = np.arange(0.5, 4.5, 0.5)
+y = np.array([7.2, 3.0, 1.5, 0.85, 0.48, 0.25, 0.20, 0.15])
