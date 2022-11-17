b8a007aead36a5f5e61c45957b330055dd57742e
kuba-swe <93076709+kuba-swe@users.noreply.github.com>
2022-11-17 15:56:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,52 +15,41 @@ import pandas as pd
 import math
 from sympy import symbols, diff
 
-def levmarq(func, x0, t, y_t):
+def levmarq(t, y_t, x0, tol=0.01, lambda_=1, alpha=1, max_iter=100):
     
-    #x1, x2 = symbols('x1 x2', real=True)
-    #F = func
-    x1 = x0[0]
-    x2 = x0[1]
+    # Initial values for parameters
+    x_k = np.array(x0)
     
-    damp = lambda_*np.eye(2)
+    # Dampening factor
+    damp = lambda_ * np.eye(2)
+    
+    # Lists for storing intermediary results
+    function_values = []
+    param_values = []
 
-    F = lambda t, x1, x2: y_t - x1*np.exp(x2*t)
+    # Functions to calculate F and gradient of F
+    F = lambda t, x_k: y_t - x_k[0] * np.exp(x_k[1] * t)
+    grad_F = lambda t, x_k: np.array([-np.exp(x_k[1] * t), -t * x_k[0] * np.exp(x_k[1] * t)])
     
-    #F_x = np.zeros(t.size)
-    F_x = [F(i, x1, x2) for i in t]
-    F_x_arr = np.array(F_x)
-    
-   # F = y_t - x1*np.exp(x2*t)
-    #grad_F = lambda t: derivative(F, x0, dx=1e-10)¨
-    n = t.size
-    grad_F = lambda t, x1, x2: np.array([-np.exp(x2*t), -t*x1*np.exp(x2*t)])
-    grad_Fx_arr = np.array([grad_F(i, x1, x2) for i in t])
-    
-   # grad_F_x1 = diff(F, x1)
-  #  grad_F_x2 = diff(F, x2)
-    
-    while n_iterations <= max_iter and minima == False:
+    # Loop until optimum or other stopping criteria reached
+    while n_iterations < max_iter:
         
-        F_x_arr = np.array([F(i, x1, x2) for i in t])
-        grad_Fx_arr = np.array([grad_F(i, x1, x2) for i in t])
+        # Save parameter values for this iteration
+        param_values.append(x_k)
         
-        Xk_1 = Xk - alpha*(grad_Fx_arr @ F_x_arr)).T @ (np.inv(F(t)* grad_F(t) + damp))
+        # Compute F(x) vectors and gradient matrix of F(x)
+        Fx = np.array([F(i, x_k) for i in t])
+        grad_Fx = np.array([grad_F(i, x_k) for i in t])
+        funcion_value = np.sum(Fx)
+        function_values.append(funcion_value)
         
-        if 
-        Xk = Xk_1
+        # Compute next point x_k+1 
+        X_k = X_k - alpha * ((grad_Fx @ Fx).T @ (np.inv(grad_Fx @ grad_Fx.T + damp)))
+        
+        if abs(grad_Fx @ Fx) < tol:
+            return (x_1, x_2)
 
-
-    return NotImplemented
-
-
-F = lambda t, x1, x2, y_t: y_t - x1*np.exp(x2*t)
+    return (x_1, x_2)
 
 t = np.arange(0.5, 4.5, 0.5)
 y = np.array([7.2, 3.0, 1.5, 0.85, 0.48, 0.25, 0.20, 0.15])
-#levmarq(F, x0, t, y)
-
-F = lambda t, x1, x2, y_t: y_t - x1*np.exp(x2*t)
-
-grad_F = lambda t: derivative(F, x0, dx=1e-10)
-
-grad_F()
