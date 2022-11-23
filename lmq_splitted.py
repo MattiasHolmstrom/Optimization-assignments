f7b6ef1e8e1baa7f67310322ef9085ae5e888686
Jakob <jakob.ma.nystrom@gmail.com>
2022-11-23 12:32:35 +0100

    Created separate methods

--- lmq_splitted.py
+++ lmq_splitted.py
@@ -16,116 +16,128 @@ from sympy import symbols, diff
 import matplotlib.pyplot as plt
 from sympy import integrate, Symbol
 
-class Lmq:
-    def __init__(self, func, gradient, x_vec, t, y):
-        self.func = func
-       # self.gradient = 
-        #self.x_vec = 
-        
-    
-    def calculate_function_val(func, y_t, t, x_k):
-        
-        func_vec = func()
-        
-        return NotImplementedError()
-    def __str__():
-        
-        return NotImplementedError()
-    def calculate_gradient(func, of_x):
-        
-        grad_f = np.array([integrate(func, x_i) for x_i in of_x])
-        print(grad_f)
-        return grad_f
 
+class LevenbergMarquardt:
 
-    f = lambda y_t, x1, x2: y_t - x1 * np.exp(x2 * t)
-
-    calculate_gradient(f, )
-
-        
-def levmarq(t, y_t, x0, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
     """
     Finds the least-squares solution for a nonlinear function using the
     Levenberg-Marquart (LM) algorithm.
 
     Args:
-        t: input arguments to the function
-        y_t: true values of the function
-        x0: starting guess for parameter values
+        func:
+        grad:
         tol: how close to zero that the gradient vector has to be for convergence
         lambda_: dampening factor used in the LM to replace the omitted term in the
             Gauss-Newton formula
         alpha: step size when updating x_k to x_k + 1
         max_iter: maximum number of iterations allowed before the algorithm terminates
-
-    Returns:
-        x_k: optimal parameter values
-        function_values: value of the function for each iteration
-        parameter_values value of parameters for each iteration
-
     """
 
-    # Initial values for parameters
-    x_k = np.array(x0)
+    def __init__(self, func, grad=None, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
 
-    # Dampening factor
-    damp = lambda_ * np.eye(2)
+        # Store input parameters as class attributes
+        self.func = func
+        self.gradient = grad
+        self.tol = tol
+        self.lambda_ = lambda_
+        self.alpha = alpha
+        self.max_iter = max_iter
 
-    # Lists for storing intermediary results
-    function_values = []
-    param_values = []
-    x_k_values = []
+        # Variables for storing results in optimization
+        self.x_k = None
+        self.function_values = []
+        self.param_values = []
+        self.final_gradient = None
 
-    # Functions to calculate F and gradient of F
-    F = lambda i, x_k: y_t[i] - x_k[0] * np.exp(x_k[1] * t[i])
-    grad_F = lambda i, x_k: np.array(
-        [
-            -np.exp(x_k[1] * t[i]),
-            -t[i] * x_k[0] * np.exp(x_k[1] * t[i])
-        ]
-    )
+        # Attribute for tracking succesful convergence
+        self.success = False
+        self.n_iterations = 0
 
-    # Initialize flag and count variables
-    n_iterations = 0
-    success = False
+    @staticmethod
+    def calculate_function_val(t, y_t, x_k):
+        """
+        Calculate the sum of least squares errors for the function supplied to the
+        optimization algorithm, for a given set of parameter values.
 
-    # Loop until optimum or other stopping criteria reached
-    while n_iterations < max_iter:
-        n_iterations += 1
+        Args:
+            t:
+            y_t:
+            x_k:
 
-        # Save parameter values for this iteration
-        param_values.append(x_k)
+        Returns:
+            Fx:
+            function_value:
+        """
 
-        # Compute F(x) vectors and gradient matrix of F(x)
+        F = lambda i, x_k: y_t[i] - x_k[0] * np.exp(x_k[1] * t[i])
         Fx = np.array([F(i, x_k) for i in range(len(t))])
-        grad_Fx = np.array([grad_F(i, x_k) for i in range(len(t))])
         function_value = Fx @ Fx.T
-        function_values.append(function_value)
-        
-        x_k_values.append(x_k)
+        return Fx, function_value
 
-        # Compute next point x_k+1
-        x_k = x_k - alpha * ((grad_Fx.T @ Fx).T @
-                             (np.linalg.inv(grad_Fx.T @ grad_Fx + damp)))
+    @staticmethod
+    def calculate_gradient(t, x_k):
 
-        if np.sum(np.abs(grad_Fx.T @ Fx)) <= tol:
-            success = True
-            break
+        grad_F = lambda i, x_k: np.array(
+            [
+                -np.exp(x_k[1] * t[i]),
+                -t[i] * x_k[0] * np.exp(x_k[1] * t[i])
+            ]
+        )
+        grad_Fx = np.array([grad_F(i, x_k) for i in range(len(t))])
+        return grad_Fx
 
-    # Print results
-    print("------ Output report ------\n")
-    print(f"Successful convergence: {success}")
-    print(f"Parameter values: {x_k}")
-    print(f"Function value: {function_values[:10]}")
-    print(f"Number of iterations: {n_iterations}")
-    print(f"Gradient vector: {grad_Fx.T @ Fx}")
-    print(f"Convergence in x: {x_k_values[-10:]}")
+    def minimize_least_squares(self, t, y_t, x0):
+        """
+        Finds the least-squares solution for a nonlinear function using the
+        Levenberg-Marquart (LM) algorithm.
 
-    return x_k, function_values, param_values
+        Args:
+            t: input arguments to the function
+            y_t: true values of the function
+            x0: starting guess for parameter values
 
-t = np.arange(0.5, 4.5, 0.5)
-y = np.array([7.2, 3.0, 1.5, 0.85, 0.48, 0.25, 0.20, 0.15])
+        Returns:
+            x_k: optimal parameter values
+            function_values: value of the function for each iteration
+            parameter_values value of parameters for each iteration
 
-levmarq(t, y, np.array([10, -5.0]))
+        """
 
-f = [np.sqrt()]
+        # Initial values for parameters
+        self.x_k = np.array(x0)
+
+        # Dampening factor
+        damp = self.lambda_ * np.eye(2)
+
+        # Initialize flag and count variables
+        self.n_iterations = 0
+
+        # Loop until optimum or other stopping criteria reached
+        while self.n_iterations < self.max_iter:
+            self.n_iterations += 1
+
+            # Save parameter values for this iteration
+            self.param_values.append(self.x_k)
+
+            # Compute F(x) vectors and gradient matrix of F(x)
+            Fx, function_value = self.calculate_function_val(t, y_t, self.x_k)
+            grad_Fx = self.calculate_gradient(t, self.x_k)
+            self.function_values.append(function_value)
+
+            # Compute next point x_k+1
+            self.x_k = self.x_k - self.alpha * ((grad_Fx.T @ Fx).T @
+                                                (np.linalg.inv(grad_Fx.T @ grad_Fx + damp)))
+
+            if np.sum(np.abs(grad_Fx.T @ Fx)) <= self.tol:
+                self.success = True
+                break
+
+    def print_output_report(self):
+
+        # Print results
+        print("------ Output report ------\n")
+        print(f"Successful convergence: {self.success}")
+        print(f"Parameter values: {self.x_k}")
+        print(f"Function value: {self.function_values[:10]}")
+        print(f"Number of iterations: {self.n_iterations}")
+        print(f"Gradient vector: {grad_Fx.T @ Fx}")
