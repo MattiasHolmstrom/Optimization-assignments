53639be3b6c14f325c547a082fc2ea869ab9b34e
Mattias Holmström <mattias.holmstrom11@gmail.com>
2022-11-28 16:45:33 +0100

    Dynamic lambda

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ def levmarq(t, y_t, x0, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
     """
 
     # Initial values for parameters
-    x_k = np.array(x0)
+    x_k = x0
+    #np.array(x0)
 
     # Dampening factor
     damp = lambda_ * np.eye(2)
@@ -48,13 +49,15 @@ def levmarq(t, y_t, x0, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
     param_values = []
 
     # Functions to calculate F and gradient of F
-    F = lambda i, x_k: y_t[i] - x_k[0] * np.exp(x_k[1] * t[i])
-    grad_F = lambda i, x_k: np.array(
-        [
-            -np.exp(x_k[1] * t[i]),
-            -t[i] * x_k[0] * np.exp(x_k[1] * t[i])
-        ]
-    )
+    def Fx(y_t, t, x_k):
+        
+        Fx = y_t - x_k[0] * np.exp(x_k[1] * t)
+        return Fx
+    
+    def grad_Fx(t, x_k):
+        
+        grad_Fx = np.array([-np.exp(x_k[1] * t), -t * x_k[0] * np.exp(x_k[1] * t)])
+        return grad_Fx
 
     # Initialize flag and count variables
     n_iterations = 0
@@ -62,24 +65,32 @@ def levmarq(t, y_t, x0, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
 
     # Loop until optimum or other stopping criteria reached
     while n_iterations < max_iter:
-        n_iterations += 1
+
 
         # Save parameter values for this iteration
         param_values.append(x_k)
 
         # Compute F(x) vectors and gradient matrix of F(x)
-        Fx = np.array([F(i, x_k) for i in range(len(t))])
-        grad_Fx = np.array([grad_F(i, x_k) for i in range(len(t))])
-        function_value = Fx @ Fx.T
+        F = Fx(y_t, t, x_k)
+        grad_F = grad_Fx(t, x_k)
+        function_value = F @ F.T
         function_values.append(function_value)
 
         # Compute next point x_k+1
-        x_k = x_k - alpha * ((grad_Fx.T @ Fx).T @
-                             (np.linalg.inv(grad_Fx.T @ grad_Fx + damp)))
+        x_k = x_k - alpha * (np.linalg.inv(grad_F @ grad_F.T + damp)) @ \
+                  (grad_F @ F)
 
-        if np.sum(np.abs(grad_Fx.T @ Fx)) <= tol:
+        if  np.sum(np.abs(grad_F @ F)) <= tol:
             success = True
             break
+        
+        if n_iterations >= 1:
+            if function_values[n_iterations] >= function_values[n_iterations-1]:
+                damp = lambda_*1 * np.eye(2)
+            else:
+                damp = lambda_/5 * np.eye(2)
+            
+        n_iterations += 1
 
     # Print results
     print("------ Output report ------\n")
@@ -87,7 +98,12 @@ def levmarq(t, y_t, x0, tol=1e-3, lambda_=1, alpha=1, max_iter=1000):
     print(f"Parameter values: {x_k}")
     print(f"Function value: {function_values[-1]}")
     print(f"Number of iterations: {n_iterations}")
-    print(f"Gradient vector: {grad_Fx.T @ Fx}")
+    print(f"Gradient vector: {grad_F @ F}")
 
     return x_k, function_values, param_values
+   
+y = np.array([7.2, 3.0, 1.5, 0.85, 0.48, 0.25, 0.20, 0.15])
+x0 = np.array([1, 1])
 
+t = np.arange(0.5, 4.5, 0.5)
+a= levmarq(t, y, x0)
